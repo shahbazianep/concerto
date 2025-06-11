3509a8e3ed5235b80acdf289975fbafdf2f5e386
Ethan S <58062201+shahbazianep@users.noreply.github.com>
2025-06-11 13:57:19 -0700

    Added analytics

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Routes, Route } from "react-router";
 import { BrowserRouter } from "react-router-dom";
+import { Analytics } from "@vercel/analytics/react";
 
 import Home from "./pages/Home.js";
 import Results from "./pages/Results.js";
@@ -9,32 +10,33 @@ import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
 import ImageRotator from "./components/ImageRotator.js";
 
 function importAll(r) {
-    let images = {};
-    r.keys().forEach((item, index) => {
-        images[item.replace("./", "")] = r(item);
-    });
-    return images;
+	let images = {};
+	r.keys().forEach((item, index) => {
+		images[item.replace("./", "")] = r(item);
+	});
+	return images;
 }
 const albumCovers = importAll(
-    require.context("./images/albumcovers", false, /\.(png|jpe?g|svg)$/)
+	require.context("./images/albumcovers", false, /\.(png|jpe?g|svg)$/)
 );
 
 export default function App() {
-    return (
-        <LocalizationProvider dateAdapter={AdapterDayjs}>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        index
-                        element={
-                            <Home
-                                children={<ImageRotator images={albumCovers} />}
-                            />
-                        }
-                    ></Route>
-                    <Route path="results" element={<Results />}></Route>
-                </Routes>
-            </BrowserRouter>
-        </LocalizationProvider>
-    );
+	return (
+		<LocalizationProvider dateAdapter={AdapterDayjs}>
+			<BrowserRouter>
+				<Routes>
+					<Route
+						index
+						element={
+							<Home
+								children={<ImageRotator images={albumCovers} />}
+							/>
+						}
+					></Route>
+					<Route path="results" element={<Results />}></Route>
+				</Routes>
+			</BrowserRouter>
+			<Analytics />
+		</LocalizationProvider>
+	);
 }
